164e40e8834bc02ae69ad0ca017134abc12791b4
divyasrivk25ai-commits <divyasrivk25ai@vetias.ac.in>
2026-06-12 15:27:11 +0530

    first commit

---
+++ src/day6.java
@@ -0,0 +1,11 @@
+public class day 6 {
+    static class car {
+        string brand;
+        int kilometer;
+        int model;
+        string col;
+    }
+    void print details()
+         System.out.println()
+
+}

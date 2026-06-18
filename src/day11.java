fff0333521d6bc908d628dfb91c6386722de00e9
divyasri <divyasrivk25ai@vetias.ac.in>
2026-06-18 09:44:53 +0530

    day11

--- src/day11.java
+++ src/day11.java
@@ -16,3 +16,19 @@ public class day11 {
     }
 }
  */
+import java.util.HashMap;
+import java.util.Map;
+
+public class day11 {
+    public static void main(String[] args) {
+        Map<String, Object> person = new HashMap<>();
+        person.put("ram", 89);
+        person.put("jhon", 78);
+        person.put("aravindh",67);
+        person.put("percent", 90);
+
+        System.out.println(person.get("ram"));
+        person.entrySet().forEach(entry ->
+            System.out.println(entry.getKey() + " : " + entry.getValue()));
+        }
+    }
